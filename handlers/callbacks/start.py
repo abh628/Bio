01a3f994131c2e5f5e399a9cf2764cc71b27a998
abh628 <rockybhaiowner@gmail.com>
2025-08-04 01:19:41 +0530

    Update start.py

--- handlers/callbacks/start.py
+++ handlers/callbacks/start.py
@@ -1,24 +1,21 @@
 from bot.bot import app
 from pyrogram import filters
-from pyrogram.types import CallbackQuery, InputMediaPhoto
+from pyrogram.types import Message
 from utils.language import get_message
 from database.user_language import get_user_language
 from config import START_IMG
 from utils.inline_buttons import start_buttons
+from database.users import store_user_data
 
-@app.on_callback_query(filters.regex("main_menu"))
-async def main_menu_cb(client, query: CallbackQuery):
-    user = query.from_user
-    lang = await get_user_language(user.id)  # ✅ await added
-    welcome_message = get_message(lang, "welcome_message").format(user=user.mention)
-
-    try:
-        await query.message.edit_media(
-            media=InputMediaPhoto(media=START_IMG, caption=welcome_message),
-            reply_markup=await start_buttons(user.id)
-        )
-    except:
-        await query.message.edit_text(
-            text=welcome_message,
-            reply_markup=await start_buttons(user.id)
-        )
+@app.on_message(filters.command("start"))
+async def start_command(client, message: Message):
+    user = message.from_user
+    full_name = f"{user.first_name or ''} {user.last_name or ''}".strip()
+    await store_user_data(user.id, user.username, full_name)
+    lang = await get_user_language(user.id)
+    welcome_text = get_message(lang, "welcome_message").format(user=user.mention)
+    await message.reply_photo(
+        photo=START_IMG,
+        caption=welcome_text,
+        reply_markup=await start_buttons(user.id)
+    )
